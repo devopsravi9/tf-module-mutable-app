43991247f2c2620723eb4a462fe4853a6ee7ca0e
Ravi <ravi@gmail.com>
2022-08-07 10:52:46 +0530

    ec2 code

--- target_group.tf
+++ target_group.tf
@@ -51,7 +51,7 @@ resource "aws_lb_listener_rule" "backend" {
 
   condition {
     host_header {
-      values = "${var.COMPONENT}-${var.ENV}.roboshop.internal"
+      values = ["${var.COMPONENT}-${var.ENV}.roboshop.internal"]
     }
   }
 }
