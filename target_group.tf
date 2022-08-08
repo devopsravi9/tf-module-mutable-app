9845cac8b57c7b8e8a128a607b6e0ba0f342ba64
Ravi <ravi@gmail.com>
2022-08-08 13:55:59 +0530

    ec2 code

--- target_group.tf
+++ target_group.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "target-group" {
   name     = "${var.COMPONENT}-${var.ENV}"
-  port     = "80"
+  port     = var.PORT
   protocol = "HTTP"
   vpc_id   = var.VPC_ID
 
@@ -17,7 +17,7 @@ resource "aws_lb_target_group_attachment" "attach" {
   count            = var.INSTANCE_COUNT
   target_group_arn = aws_lb_target_group.target-group.arn
   target_id        = aws_spot_instance_request.instance.*.spot_instance_id[count.index]
-  port             = "80"
+  port             = var.PORT
 }
 
 resource "aws_lb_listener" "frontend" {
